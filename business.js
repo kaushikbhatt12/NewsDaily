68a25bd6388cb308051f1525995d5248dd8dfd75
kaushik <kaushikbhat791@gmail.com>
2022-01-23 17:22:29 +0530

    first commit

---
+++ business.js
@@ -0,0 +1,50 @@
+// let source='the-times-of-india'
+// let api='5af1b1917bdb4477a22fb76402546025'
+// function getdata(){
+//     url=`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5af1b1917bdb4477a22fb76402546025`;
+//     fetch(url).then((response)=>{
+//         return response.json();
+//     }).then((data)=>{
+//         console.log(data);
+  
+//     })
+// }
+// getdata()
+
+
+
+let newsHtml="";
+c=1;
+console.log('hola')
+//initialize news parameter
+let source = 'techcrunch';
+let apiKey = '5af1b1917bdb4477a22fb76402546025';
+//grab news container
+let space = document.getElementById('demo');
+// create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5af1b1917bdb4477a22fb76402546025`, true);
+
+//what to do when response is ready
+xhr.onload = function () {
+  if (this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    // console.log(articles);
+    let newsHtml = "";
+    articles.forEach(function (element) {
+      
+      // let news = element["title"]
+      // let urrl=element["url"]
+      // let description=element["description"]
+      // console.log(news)
+      newsHtml+=`${c}. ${element["title"]} <a href=${element["url"]} target=_blank>Read here </a><br><br>`
+      c+=1;
+    });
+    space.innerHTML = newsHtml;
+  }
+  else {
+    console.log("error")
+  }
+}
+xhr.send()
---
+++ politics.js
@@ -0,0 +1,32 @@
+let newsHtml="";
+c=1;
+console.log('hola')
+//initialize news parameter
+let source = 'techcrunch';
+let apiKey = '5af1b1917bdb4477a22fb76402546025';
+//grab news container
+let space = document.getElementById('demo');
+// create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=in&category=politics&apiKey=5af1b1917bdb4477a22fb76402546025`, true);
+
+//what to do when response is ready
+xhr.onload = function () {
+  if (this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+    // console.log(articles);
+    let newsHtml = "";
+    articles.forEach(function (element) {
+      
+  
+      newsHtml+=`${c}. ${element["title"]} <a href=${element["url"]} target=_blank>Read here </a><br><br>`
+      c+=1;
+    });
+    space.innerHTML = newsHtml;
+  }
+  else {
+    console.log("error")
+  }
+}
+xhr.send()
